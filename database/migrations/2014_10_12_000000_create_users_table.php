759dddd58e0d440de875deaa6d24df360f91ff3e
Agustin Amato <agustin.amato96@gmail.com>
2020-10-09 12:26:18 -0300

    user migration ajuste menor

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->increments('id');
             $table->string('name');
             $table->string('email', 100)->unique();
@@ -21,7 +22,6 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->engine = 'InnoDB';
         });
     }
 
