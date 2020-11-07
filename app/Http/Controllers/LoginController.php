4a80cca87e0894c0cac5f5304e6fed5a2f9c3ff0
Agustin Amato <agustin.amato96@gmail.com>
2020-11-07 03:13:54 -0300

    [login] agrego token y validations

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Commerce;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -17,33 +19,47 @@ class LoginController extends Controller
      */
     public function authenticate(Request $request)
     {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'nullable',
+        ]);
+
         $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
-            $rsp = Auth::user();
-            $rsp->commerces = Commerce::ofUser()->get();
-
-            return response()->json($rsp);
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['error' => 'User not found'], 401);
         }
 
-        return response()->json(['error' => 'User not found']);
+        $user = User::find(Auth::user()->id);
+        $user->commerces = Commerce::ofUser()->get();
+        $user->token = $user->createToken($request->device_name ?? 'token-name')->plainTextToken;
+
+        return response()->json($user);
     }
 
     public function me(Request $request)
     {
-        if (Auth::user()) {
-            $rsp = Auth::user();
-            $rsp->commerces = Commerce::ofUser()->get();
-
-            return response()->json($rsp);
+        if (!Auth::user()) {
+            return response()->json(['error' => 'error']);
         }
+
+        $rsp = Auth::user();
+        $rsp->commerces = Commerce::ofUser()->get();
+
+        return response()->json($rsp);
     }
 
     public function logout(Request $request)
     {
-        if (Auth::user()) {
-            return response()->json(Auth::logout());
+        if (!Auth::user()) {
+            return response()->json(['error' => 'error']);
         }
+
+        $user = User::find(Auth::user()->id);
+        if ($user->currentAccessToken()) $user->currentAccessToken()->delete();
+
+        return response()->json(Auth::logout());
     }
 
     /**
