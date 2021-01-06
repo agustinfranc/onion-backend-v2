a5c805a70f84b650ee58bf975bc2432d7a8d12e3
Agustin Amato <agustin.amato96@gmail.com>
2021-01-06 15:02:49 -0300

    agrego columna slideable a commerce_subrubro

---
+++ database/migrations/2021_01_06_175432_delete_with_slider_from_commerces_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DeleteWithSliderFromCommercesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('commerces', function (Blueprint $table) {
+            $table->dropColumn('with_slider');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('commerces', function (Blueprint $table) {
+            $table->boolean('with_slider')->default(1)->after('avatar_dirname');
+        });
+    }
+}
