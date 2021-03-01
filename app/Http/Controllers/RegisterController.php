758d2a427cd2af22c03ae7a12f0ff1a95f3d7dca
Agustin Amato <agustin.amato96@gmail.com>
2021-03-01 11:03:38 -0300

    API: Agrego ruta de registro

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request, User $user)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',      //confirmed === password_confirmation
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $user->token = $user->createToken($validatedData['device_name'] ?? 'token-name')->plainTextToken;
+
+        return $user;
+    }
+
+}
