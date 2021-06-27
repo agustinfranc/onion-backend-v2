180dfd2cc096ea772b2ed70edfbcca27f23d4e12
Agustin Amato <agustin.amato96@gmail.com>
2021-06-27 16:38:43 -0300

    updated Factories

--- database/factories/CommerceFactory.php
+++ database/factories/CommerceFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Commerce;
+use App\Models\Currency;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -23,11 +24,11 @@ class CommerceFactory extends Factory
     public function definition()
     {
         return [
-            'name' => Str::slug($this->faker->name),
-            'fullname' => $this->faker->name,
+            'name' => Str::slug($this->faker->catchPhrase),
+            'fullname' => $this->faker->catchPhrase,
             'cover_dirname' => 'https://picsum.photos/510/300?random',
             'avatar_dirname' => 'https://picsum.photos/510/300?random',
-            'currency_id' => 1,
+            'currency_id' => Currency::factory(),
             'dark_theme' => 1,
             'has_action_buttons' => 0,
             'has_footer' => 1,
