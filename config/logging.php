a466a28440914c090d323bd302f29631b5eb4c70
Agustin Amato <agustin.amato96@gmail.com>
2023-04-17 00:10:14 -0300

    feat: add papertrail to stack log channels

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'papertrail'],
             'ignore_exceptions' => false,
         ],
 
