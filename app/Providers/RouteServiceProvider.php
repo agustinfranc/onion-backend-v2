759c8f67c7e89da42489229a350278884d66c39e
Agustin Amato <agustin.amato96@gmail.com>
2023-12-18 23:22:12 -0300

    feat: add welcome view

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -43,6 +43,9 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
         });
     }
 }
