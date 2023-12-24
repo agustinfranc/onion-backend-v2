d1ffb90fdce384f6cfd85ecb43ac2e2221281d56
Agustin Amato <agustin.amato96@gmail.com>
2023-12-24 14:45:41 -0300

    feat: redirect to dashboards

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/dashboard');
+    // return view('welcome/');
 });
 
 Route::get('/dashboard', function () {
@@ -28,4 +29,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
